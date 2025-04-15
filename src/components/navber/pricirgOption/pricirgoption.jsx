642af5b32bc56319e574ea7350d4acc28514465e
Topan-Roy <roytopan734@gmail.com>
2025-04-15 13:21:22 +0600

    added dasycrad

--- src/components/navber/pricirgOption/pricirgoption.jsx
+++ src/components/navber/pricirgOption/pricirgoption.jsx
@@ -1,5 +1,7 @@
 import React, { use } from 'react';
 import PricingCard from '../../../pricingCard/pricingCard';
+import Dasyprincig from '../../../dasyPrincingCard/dasyprincig';
+
 
 const Pricirgoption = ({pricingPromice}) => {
     const pricingData=use(pricingPromice);
@@ -7,8 +9,11 @@ const Pricirgoption = ({pricingPromice}) => {
         <div>
             <h1 className='text-2xl'>Get our membership</h1>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
-                {
+                {/* {
                     pricingData.map(pricing=><PricingCard key={pricing.id} pricing={pricing}></PricingCard>)
+                } */}
+                {
+                   pricingData.map(pricing=><Dasyprincig key={pricing.id} pricing={pricing}></Dasyprincig>)
                 }
             </div>
         </div>
